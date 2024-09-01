d6c9d87f543a293014d2759de0b14ba56405ff5c
jrafagnin <jfbr.pessoal@gmail.com>
2024-09-01 20:47:42 -0300

    Js update

--- Curso C/Secao 05/ex01.c
+++ Curso C/Secao 05/ex01.c
@@ -3,9 +3,13 @@
 // 1 - Faça um programa que determine e mostre os cinco primeiros múltiplos de 3, considerando números 
 // maiores que 0.
 
-// 2 - Write a program that determines and displays the first five multiples of 3, considering numbers greater than 0.
+// 1 - Write a program that determines and displays the first five multiples of 3, considering numbers greater than 0.
 
 int main (){
- 
+ double num, num2;
+ printf("Informe o numero selecionado:");
+ scanf("%f", &num);
+
+num2 = nu
     return 0;
 }
