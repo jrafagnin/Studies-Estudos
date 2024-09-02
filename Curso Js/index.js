536f7535365436312b748afe028fb95786c7d43a
jrafagnin <jfbr.pessoal@gmail.com>
2024-09-02 18:42:23 -0300

    Js course update

---
+++ Curso Js/index.js
@@ -0,0 +1,43 @@
+//this is my first javascript code!
+console.log('Hello World')
+
+//------VARIABLES----------------------------------------------------------------------
+//let name = 'Mosh';
+//console.log(name);
+//Cannot be a reserved keyword
+//Should be meaningful
+//Cannot start with a number (e.g. "1name")
+//Cannot contain a space or hyphen (-)
+//Are Case-sensitive
+//let firstName != let Firstname
+//For best practice declare each variables in a single line
+//let firstName = 'Mosh';
+//let lastName = 'Hamedani';
+// ---------------------------------------------------------------------------------------
+
+
+
+// ------CONSTANTS------------------------------------------------------------------------
+//let interestRate = 0.3; <- LET If you need to reasign a variable  *****
+//interestRate = 1;
+//console.log(interestRate);
+
+//Const error
+//const interestRate = 0.3; <- CONST If you dont need to reasign a variable *****
+//interestRate = 1; <- Reasign a const will cause error (Assignment to constant variable)
+//console.log(interestRate);
+// ---------------------------------------------------------------------------------------
+
+
+
+// ------ PRIMITIVES / VALUE TYPES -------------------------------------------------------
+// String / Number / Boolean / Undefined / Null
+let name = 'Mosh'; // String literal
+let age = 30; // Number literal
+let isApproved = false; // Boolean Literal
+let firstName=undefined; let firstname; // Undefined
+let selectedColor = null; // Null <-- When you want to let a variable without value;
+// ---------------------------------------------------------------------------------------
+
+
+// ------ REFERENCE TYPES ----------------------------------------------------------------
